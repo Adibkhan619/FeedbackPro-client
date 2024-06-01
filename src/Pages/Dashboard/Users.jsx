514ae1bb80611c218b3257df8f36a882af6fe733
Adibkhan619 <work.adibkhan@gmail.com>
2024-06-02 00:49:38 +0600

    Added filter into user page

--- src/Pages/Dashboard/Users.jsx
+++ src/Pages/Dashboard/Users.jsx
@@ -3,20 +3,21 @@ import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { HiTrash } from "react-icons/hi";
 import { FaIdBadge, FaRegIdBadge, FaUsers } from "react-icons/fa";
 import Swal from "sweetalert2";
+import { useState } from "react";
 
 const Users = () => {
+    const [role, setRole] = useState(false);
+    // const [filterUser, setFilterUser] = useState(users)
     const axiosSecure = useAxiosSecure();
-    const {data: users =[], refetch} = useQuery({
-        queryKey: ['users'],
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ["users"],
         queryFn: async () => {
-            const res = await axiosSecure.get('/users')
+            const res = await axiosSecure.get("/users");
             return res.data;
-        }
-        
+        },
+    });
 
-    })
-
-    const handleMakeAdmin =(user) => {
+    const handleMakeAdmin = (user) => {
         axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
             console.log(res.data);
             if (res.data.modifiedCount > 0) {
@@ -39,16 +40,18 @@ const Users = () => {
                     },
                 });
             }
-        })
-    }
+        });
+    };
 
-    const handleMakeSurveyor =(user) => {
+    const handleMakeSurveyor = (user) => {
         axiosSecure.patch(`/users/surveyor/${user._id}`).then((res) => {
             console.log(res.data);
             if (res.data.modifiedCount > 0) {
                 refetch();
                 Swal.fire({
-                    title: `${user.displayName || user.name} is a Surveyor Now!`,
+                    title: `${
+                        user.displayName || user.name
+                    } is a Surveyor Now!`,
                     showClass: {
                         popup: `
                         animate__animated
@@ -65,13 +68,35 @@ const Users = () => {
                     },
                 });
             }
-        })
-    }
+        });
+    };
+
+    const filteredUsers = users.filter((user) => user.role == role);
+
+    const handleRole = (e) => {
+        setRole(e.target.value);
+        console.log(role);
+    };
+
     return (
         <div>
-            <h1>users: {users.length}</h1>
+            
+
             <div className="border mx-24 p-5">
-                <h1 className="text-5xl  my-5 ">Total Users: {users.length}</h1>
+                <div className="flex justify-between items-center">
+            <h1 className="text-5xl  my-5 ">Total Users: {users.length}</h1>
+            <select
+                onChange={handleRole}
+                className="select select-bordered w-full max-w-xs"
+            >
+                <option value="">All</option>
+                <option value="admin">Admin</option>
+                <option value="surveyor">Surveyor</option>
+                <option value="user">User</option>
+                <option value="proUser">Pro User</option>
+            </select>
+            </div>
+
                 <div className="overflow-x-auto ">
                     <table className="table ">
                         {/* head */}
@@ -87,41 +112,95 @@ const Users = () => {
                         </thead>
                         <tbody>
                             {/* row 1 */}
-                            {users.map((user, idx) => (
-                                <tr key={user._id}>
-                                    <th>{idx + 1}</th>
-                                    <td>{user.name}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.role}</td>
-                                    
-                                    <td>
-                                        { user.role ===('surveyor') || user.role ===('admin') ? ' ' :  
-                                        <button
-                                            onClick={() =>
-                                                handleMakeSurveyor(user)
-                                            }
-                                            className="btn bg-orange-200"
-                                        > Make Surveyor
-                                            <FaUsers className="text-2xl text-white  " />
-                                        </button>
-                                        }                                       
-                                    </td>
-                                    <td>
-                                        { user.role === ( 'admin') ? ' ' :  
-                                        <button
-                                            onClick={() =>
-                                                handleMakeAdmin(user)
-                                            }
-                                            className="btn bg-orange-200"
-                                        > Make Admin
-                                            <FaRegIdBadge className="text-2xl text-white  " />
-                                        </button>
-                                        }                                       
-                                    </td>
+                            {!role
+                                ? users.map((user, idx) => (
+                                      <tr key={user._id}>
+                                          <th>{idx + 1}</th>
+                                          <td>{user.name}</td>
+                                          <td>{user.email}</td>
+                                          <td>{user.role}</td>
 
-                                
-                                </tr>
-                            ))}
+                                          <td>
+                                              {user.role === "surveyor" ||
+                                              user.role === "admin" ? (
+                                                  " "
+                                              ) : (
+                                                  <button
+                                                      onClick={() =>
+                                                          handleMakeSurveyor(
+                                                              user
+                                                          )
+                                                      }
+                                                      className="btn bg-orange-200"
+                                                  >
+                                                      {" "}
+                                                      Make Surveyor
+                                                      <FaUsers className="text-2xl text-white  " />
+                                                  </button>
+                                              )}
+                                          </td>
+                                          <td>
+                                              {user.role === "admin" ? (
+                                                  " "
+                                              ) : (
+                                                  <button
+                                                      onClick={() =>
+                                                          handleMakeAdmin(user)
+                                                      }
+                                                      className="btn bg-orange-200"
+                                                  >
+                                                      {" "}
+                                                      Make Admin
+                                                      <FaRegIdBadge className="text-2xl text-white  " />
+                                                  </button>
+                                              )}
+                                          </td>
+                                      </tr>
+                                  ))
+                                : filteredUsers.map((user, idx) => (
+                                      <tr key={user._id}>
+                                          <th>{idx + 1}</th>
+                                          <td>{user.name}</td>
+                                          <td>{user.email}</td>
+                                          <td>{user.role}</td>
+
+                                          <td>
+                                              {user.role === "surveyor" ||
+                                              user.role === "admin" ? (
+                                                  " "
+                                              ) : (
+                                                  <button
+                                                      onClick={() =>
+                                                          handleMakeSurveyor(
+                                                              user
+                                                          )
+                                                      }
+                                                      className="btn bg-orange-200"
+                                                  >
+                                                      {" "}
+                                                      Make Surveyor
+                                                      <FaUsers className="text-2xl text-white  " />
+                                                  </button>
+                                              )}
+                                          </td>
+                                          <td>
+                                              {user.role === "admin" ? (
+                                                  " "
+                                              ) : (
+                                                  <button
+                                                      onClick={() =>
+                                                          handleMakeAdmin(user)
+                                                      }
+                                                      className="btn bg-orange-200"
+                                                  >
+                                                      {" "}
+                                                      Make Admin
+                                                      <FaRegIdBadge className="text-2xl text-white  " />
+                                                  </button>
+                                              )}
+                                          </td>
+                                      </tr>
+                                  ))}
                         </tbody>
                     </table>
                 </div>
